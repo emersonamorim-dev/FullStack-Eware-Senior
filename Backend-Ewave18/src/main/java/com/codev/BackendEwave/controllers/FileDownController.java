462ccd0a94ff347149dc9d4a47f06c4c948b6e20
emersonamorim-dev <emerson_tecno@hotmail.com>
2023-09-11 11:50:58 -0300

    Aplicação Backend Eware - Java e SpringBoot 🚀 🔄 🌐

--- Backend-Ewave18/src/main/java/com/codev/BackendEwave/controllers/FileDownController.java
+++ Backend-Ewave18/src/main/java/com/codev/BackendEwave/controllers/FileDownController.java
@@ -12,6 +12,7 @@ import org.springframework.core.io.AbstractFileResolvingResource;
 import org.springframework.core.io.AbstractResource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,7 @@ import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")   
+@CrossOrigin(origins = "http://localhost:4200")
 public class FileDownController {
 
     @Autowired
